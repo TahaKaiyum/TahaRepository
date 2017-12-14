95ecfd75746c59d89f4f37bdf91f6f00eb91a1e4
TahaKaiyum <33505733+TahaKaiyum@users.noreply.github.com>
2017-12-14 12:01:27 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,59 +1,64 @@
-package bankinfo;
+package org.main;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import org.auth.UserCredential;
+import org.auth.UserProfile;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class Solution {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) {
 		SessionFactory sf = new Configuration().configure().buildSessionFactory();
 		Session session = sf.openSession();
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		double amt;
-		int nooftrans;
-		int i=0;
-		while(i<2)
-		{
-	    System.out.println("Enter your account number: ");
-		long accountNumber=Long.valueOf(bf.readLine());
-		System.out.println("Enter the account holder name: ");
-		String accountHolder=bf.readLine();
-		System.out.println("Enter your address: ");
-		String address=bf.readLine();
-		System.out.println("Enter your phone number:  ");
-		long phoneNumber=Long.valueOf(bf.readLine());
-		System.out.println("Enter your mail id:  ");
-		String mailId=bf.readLine();
-		System.out.println("Enter your balance ");
-		double balance=Double.valueOf(bf.readLine());
-		
-		Bankaccount ba=new Bankaccount(accountNumber, accountHolder, address, phoneNumber, mailId, balance);
-		
-	    session.save(ba);
 		session.beginTransaction();
+		
+		UserCredential steve = new UserCredential();
+		steve.setUsername("steve");
+		steve.setPassword("jobs");
+		steve.setStatus(0);
+		
+		UserCredential guido = new UserCredential();
+		guido.setUsername("guido");
+		guido.setPassword("rossum");
+		guido.setStatus(0);
+		
+		UserProfile profile1 = new UserProfile();
+		profile1.setFirstName("Steve");
+		profile1.setLastName("Jobs");
+		profile1.setEmailId("steve@apple.com");
+		profile1.setPhoneNo("9876543210");
+		
+		UserProfile profile2 = new UserProfile();
+		profile2.setFirstName("Guido Van");
+		profile2.setLastName("Rossum");
+		profile2.setEmailId("guido@python.org");
+		profile2.setPhoneNo("9876543211");
+		
+		UserProfile profile3 = new UserProfile();
+		profile3.setFirstName("James");
+		profile3.setLastName("Gosling");
+		profile3.setEmailId("james@java.com");
+		profile3.setPhoneNo("9876543212");
+		
+		steve.getProfiles().add(profile1);
+		steve.getProfiles().add(profile3);
+		guido.getProfiles().add(profile2);
+		
+		profile1.setCredential(steve);
+		profile2.setCredential(guido);
+		
+		
+		session.save(steve);
+		session.save(guido);
+		session.save(profile1);
+		session.save(profile2);
+		session.save(profile3);
+		
 		session.getTransaction().commit();
-		i++;}
-
 		session.close();
 		sf.close();
-		System.out.println("1.Savings account\n 2.Current account");
-		int n=Integer.valueOf(bf.readLine());
-		switch(n)
-		{
-		case 1:
-			amt=Double.valueOf(bf.readLine());
-			nooftrans=Integer.valueOf(bf.readLine());
-		}
-	}
-	
-	
-	
 
-	
+	}
 
 }
